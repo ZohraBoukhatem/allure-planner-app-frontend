bc7702a6038cb9a19907c8c266758ec0b801d1ce
Zohra Boukhatem <zohra.boukhatem1@gmail.com>
2024-02-25 01:21:08 +0100

    style details page

--- src/Pages/EditWedding.jsx
+++ src/Pages/EditWedding.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
+import "./NewWedding.css"
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -148,252 +149,261 @@ useEffect(() => {
 
   
   return (
-    <>
-    {loading 
-      ? <img src="/logoGif.gif" alt="animatedLogo" />
-      :
-      <div className="justify">
-        <form>
-          <label>The bride: </label>
+    <form onSubmit={handleEdit}>
+      <img src="/A.png" alt="logo" />
+
+      <div className="newWedding">
+        <label>The bride: </label>
+        <input
+          type="text"
+          placeholder="First name..."
+          name="brideFirstName"
+          value={brideFirstName}
+          onChange={(e) => setBrideFirstName(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Last name..."
+          name="brideLastName"
+          value={brideLastName}
+          onChange={(e) => setBrideLastName(e.target.value)}
+        />
+
+        <label>The groom: </label>
+        <input
+          type="text"
+          placeholder="First name..."
+          name="groomFirstName"
+          value={groomFirstName}
+          onChange={(e) => setGroomFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Last name..."
+          name="groomLastName"
+          value={groomLastName}
+          onChange={(e) => setGroomLastName(e.target.value)}
+        />
+
+        <label>The date: </label>
+        <input
+          type="text"
+          placeholder="yyyy-mm-dd"
+          name="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+
+        <label>The venues: </label>
+        <div>
           <input
             type="text"
-            placeholder="First name..."
-            name="brideFirstName"
-            value={brideFirstName}
-            onChange={(e) => setBrideFirstName(e.target.value)}
-          />
-
-          <input
-            type="text"
-            placeholder="Last name..."
-            name="brideLastName"
-            value={brideLastName}
-            onChange={(e) => setBrideLastName(e.target.value)}
-          />
-
-          <label>The groom: </label>
-          <input
-            type="text"
-            placeholder="First name..."
-            name="groomFirstName"
-            value={groomFirstName}
-            onChange={(e) => setGroomFirstName(e.target.value)}
+            placeholder="Venue Name..."
+            name="venueName"
+            value={venueName}
+            onChange={(e) => setVenueName(e.target.value)}
           />
           <input
             type="text"
-            placeholder="Last name..."
-            name="groomLastName"
-            value={groomLastName}
-            onChange={(e) => setGroomLastName(e.target.value)}
+            placeholder="Venue address..."
+            name="venueAddress"
+            value={venueAddress}
+            onChange={(e) => setVenueAddress(e.target.value)}
           />
-
-          <label>The date: </label>
           <input
             type="text"
-            placeholder="yyyy-mm-dd"
-            name="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
+            placeholder="Venue number..."
+            name="venueTel"
+            value={venueTel}
+            onChange={(e) => setVenueTel(e.target.value)}
           />
+          <button onClick={addVenue}>Add venue</button>
+        </div>
+        {venues.length > 0 && 
+          <div className="venues">
+            <ul>
+              {venues.map((elm) => {
+                return (
+                  <div className="venue">
+                    <li key={elm.venueName}>{elm.venueName}</li>
+                    <button
+                      className="remove"
+                      onClick={(e) => venueDelete(e, elm.venueName)}
+                    >
+                      x
+                    </button>
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
+        }
 
-          <label>The venues: </label>
-          <div className="">
-            <input
-              type="text"
-              placeholder="Venue Name..."
-              name="venueName"
-              value={venueName}
-              onChange={(e) => setVenueName(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Venue address..."
-              name="venueAddress"
-              value={venueAddress}
-              onChange={(e) => setVenueAddress(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Venue number..."
-              name="venueTel"
-              value={venueTel}
-              onChange={(e) => setVenueTel(e.target.value)}
-            />
-            <button onClick={addVenue}>Add venue</button>
-          </div>
-          <div className="guestAdd">
-            <div>
-              <h5>Venues</h5>
-              <ul>
-                {venues.map((elm) => {
-                  return (
-                    <>
-                      <li key={elm.venueName}>{elm.venueName}</li>
-                      <button onClick={(e) => venueDelete(e, elm.venueName)}>
-                        x
-                      </button>
-                    </>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
+        <label>The budget: </label>
+        <div className="budget">
+          <input
+            type="text"
+            placeholder="Budget..."
+            name="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <select
+            name="currency"
+            value={currency}
+            onChange={(e) => setCurrency(e.target.value)}
+          >
+            <option>EUR</option>
+            <option>USD</option>
+            <option>KN</option>
+          </select>
+        </div>
 
-          <label>The budget: </label>
-          <div className="guest">
-            <input
-              type="text"
-              placeholder="Budget..."
-              name="amount"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-            />
-            <select
-              name="currency"
-              value={currency}
-              onChange={(e) => setCurrency(e.target.value)}
-            >
-              <option>EUR</option>
-              <option>USD</option>
-              <option>KN</option>
-            </select>
-          </div>
-
-          <label>Vendors: </label>
-          <div className="">
-            <input
-              type="text"
-              placeholder="Vendor name..."
-              name="vendorName"
-              value={vendorName}
-              onChange={(e) => setVendorName(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Vendor address..."
-              name="vendorAddress"
-              value={vendorAddress}
-              onChange={(e) => setVendorAddress(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Vendor number..."
-              name="vendorTel"
-              value={vendorTel}
-              onChange={(e) => setVendorTel(e.target.value)}
-            />
-            <button onClick={addVendor}>Add vendor</button>
-          </div>
-          <div className="guestAdd">
-            <div>
-              <h5>Vendors</h5>
-              <ul>
-                {vendors.map((elm) => {
-                  return (
-                    <>
-                      <li key={elm.vendorName}>{elm.vendorName}</li>
-                      <button onClick={(e) => vendorDelete(e, elm.vendorName)}>
-                        x
-                      </button>
-                    </>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-
-          <label>The schedule: </label>
-          <div className="schedule">
-            <label>Reception: </label>
-            <input
-              type="time"
-              placeholder="h"
-              name="reception"
-              value={reception}
-              onChange={(e) => setReception(e.target.value)}
-            />
-            <label>Ceremony: </label>
-            <input
-              type="time"
-              placeholder="h"
-              name="ceremony"
-              value={ceremony}
-              onChange={(e) => setCeremony(e.target.value)}
-            />
-            <label>Afterparty: </label>
-            <input
-              type="time"
-              placeholder="h"
-              name="afterparty"
-              value={afterparty}
-              onChange={(e) => setAfterparty(e.target.value)}
-            />
-          </div>
-
-          <label>The guests: </label>
-          <div className="guest">
-            <input
-              type="text"
-              placeholder="Name"
-              name="guestName"
-              value={guestName}
-              onChange={(e) => setGuestName(e.target.value)}
-            />
-            <select
-              name="link"
-              value={link}
-              onChange={(e) => setLink(e.target.value)}
-            >
-              <option>bride's</option>
-              <option>groom's</option>
-            </select>
-            <button type="submit" onClick={addGuest}>
-              Add guest
-            </button>
-          </div>
-          <div className="guestAdd">
-            <div>
-              <h5>Bride's</h5>
-              <ul>
-                {guestList.map((elm) => {
-                  if (elm.link === "bride's") {
-                    return (
-                      <>
-                        <li key={elm.guestName}> {elm.guestName}</li>
-                        <button onClick={(e) => guestDelete(e, elm.guestName)}>
-                          x
-                        </button>
-                      </>
-                    );
-                  }
-                })}
-              </ul>
-            </div>
-            <div>
-              <h5>Groom's</h5>
-              <ul>
-                {guestList.map((elm) => {
-                  if (elm.link === "groom's") {
-                    return (
-                      <>
-                        <li key={elm.guestName}> {elm.guestName}</li>
-                        <button onClick={(e) => guestDelete(e, elm.guestName)}>
-                          x
-                        </button>
-                      </>
-                    );
-                  }
-                })}
-              </ul>
-            </div>
-          </div>
-          <button type="submit" onClick={handleEdit}>
-            Edit Wedding
-          </button>
-        </form>
-      </div>
+        <label>Vendors: </label>
+        <div>
+          <input
+            type="text"
+            placeholder="Vendor name..."
+            name="vendorName"
+            value={vendorName}
+            onChange={(e) => setVendorName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Vendor address..."
+            name="vendorAddress"
+            value={vendorAddress}
+            onChange={(e) => setVendorAddress(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Vendor number..."
+            name="vendorTel"
+            value={vendorTel}
+            onChange={(e) => setVendorTel(e.target.value)}
+          />
+          <button onClick={addVendor}>Add vendor</button>
+        </div>
+        {vendors.length > 0 &&
+        <div className="vendors">
+          <ul>
+            {vendors.map((elm) => {
+              return (
+                <div className="vendor">
+                  <li key={elm.vendorName}>{elm.vendorName}</li>
+                  <button
+                    className="remove"
+                    onClick={(e) => vendorDelete(e, elm.vendorName)}
+                  >
+                    x
+                  </button>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
 }
-    </>
+
+        <label>The schedule: </label>
+        <div className="schedule">
+          <label>Reception: </label>
+          <input
+            type="time"
+            placeholder="h"
+            name="reception"
+            value={reception}
+            onChange={(e) => setReception(e.target.value)}
+          />
+          <label>Ceremony: </label>
+          <input
+            type="time"
+            placeholder="h"
+            name="ceremony"
+            value={ceremony}
+            onChange={(e) => setCeremony(e.target.value)}
+          />
+          <label>Afterparty: </label>
+          <input
+            type="time"
+            placeholder="h"
+            name="afterparty"
+            value={afterparty}
+            onChange={(e) => setAfterparty(e.target.value)}
+          />
+        </div>
+
+        <label>The guests: </label>
+        <div className="guestAdd">
+          <input
+            type="text"
+            placeholder="Name"
+            name="guestName"
+            value={guestName}
+            onChange={(e) => setGuestName(e.target.value)}
+          />
+          <select
+            name="link"
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          >
+            <option>bride's</option>
+            <option>groom's</option>
+          </select>
+          </div>
+          <button type="submit" onClick={addGuest}>
+            Add guest
+          </button>
+         {guestList.length > 0 &&
+        <div className="guests">
+         
+            <ul>
+            <h5>Bride's</h5>
+              {guestList.map((elm) => {
+                if (elm.link === "bride's") {
+                  return (
+                    <div className="guest">
+                      <li key={elm.guestName}> {elm.guestName}</li>
+                      <button
+                        className="remove"
+                        onClick={(e) => guestDelete(e, elm.guestName)}
+                      >
+                        x
+                      </button>
+                    </div>
+                  );
+                }
+              })}
+            </ul>
+    
+       
+       <ul>
+              <h5>Groom's</h5>
+              {guestList.map((elm) => {
+                if (elm.link === "groom's") {
+                  return (
+                    <div className="guest">
+                      <li key={elm.guestName}> {elm.guestName}</li>
+                      <button
+                        className="remove"
+                        onClick={(e) => guestDelete(e, elm.guestName)}
+                      >
+                        x
+                      </button>
+                    </div>
+                  );
+                }
+              })}
+            </ul>
+          </div>
+        
+      }
+           
+      <hr/>
+
+        <button className="submitButton" type="submit">Edit wedding</button>
+      </div>
+    </form>
   );
 }
 
